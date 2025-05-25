190b1759d06228a109b5c2d2c2c6e1794d7d5a91
Rasim Aghayev <rasimaqayev@gmail.com>
2025-05-25 16:48:40 +0400

    Refactor: Improve HoverCard component structure and type definitions for better clarity
    - Replaced React.forwardRef with named forwardRef imports for consistency.
    - Updated imports to use named imports for HoverCard components from @radix-ui/react-hover-card.
    - Enhanced type definitions for HoverCard components including Content and Trigger.
    - Improved formatting and consistency of string quotes across the file.

--- src/components/ui/hover-card.tsx
+++ src/components/ui/hover-card.tsx
@@ -1,18 +1,21 @@
+import {
+  forwardRef,
+  type ElementRef,
+  type ComponentPropsWithoutRef,
+} from "react";
+import { Root, Trigger, Content } from "@radix-ui/react-hover-card";
 
-import * as React from "react"
-import * as HoverCardPrimitive from "@radix-ui/react-hover-card"
+import { cn } from "@/lib/utils";
 
-import { cn } from "@/lib/utils"
+const HoverCard = Root;
 
-const HoverCard = HoverCardPrimitive.Root
+const HoverCardTrigger = Trigger;
 
-const HoverCardTrigger = HoverCardPrimitive.Trigger
-
-const HoverCardContent = React.forwardRef<
-  React.ElementRef<typeof HoverCardPrimitive.Content>,
-  React.ComponentPropsWithoutRef<typeof HoverCardPrimitive.Content>
+const HoverCardContent = forwardRef<
+  ElementRef<typeof Content>,
+  ComponentPropsWithoutRef<typeof Content>
 >(({ className, align = "center", sideOffset = 4, ...props }, ref) => (
-  <HoverCardPrimitive.Content
+  <Content
     ref={ref}
     align={align}
     sideOffset={sideOffset}
@@ -22,7 +25,7 @@ const HoverCardContent = React.forwardRef<
     )}
     {...props}
   />
-))
-HoverCardContent.displayName = HoverCardPrimitive.Content.displayName
+));
+HoverCardContent.displayName = Content.displayName;
 
-export { HoverCard, HoverCardTrigger, HoverCardContent }
+export { HoverCard, HoverCardTrigger, HoverCardContent };
